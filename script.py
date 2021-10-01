5522f39f538a74245ee111524902320443080707
Furkan Sahin <furkansahin824@gmail.com>
2021-10-01 16:28:33 -0500

    added function to cleanup csv

--- script.py
+++ script.py
@@ -1,4 +1,18 @@
+from os import fsdecode, read
 import psycopg2
+import csv
+import pandas as pd
+
+
+def cleanup(csv_file, dir='data/'):
+    data = pd.read_csv(dir + csv_file + '.csv', sep='\t')
+    del data['Unnamed: 0']
+    # print(f"Cleaned up {csv_file}.csv:")
+    # print(data)
+    return data
+
+def addTable(csv):
+    print(csv)
 
 # 315_psql='psql -h csce-315-db.engr.tamu.edu -U csce315_913_3_user -d csce315_913_3_db'
 conn = psycopg2.connect(
@@ -8,7 +22,13 @@ conn = psycopg2.connect(
     password="sikewrongnumber"
 )
 
+addTable(cleanup('crew'))
+cleanup('customer_ratings')
+cleanup('names')
+cleanup('principals')
+cleanup('titles')
+
 cur = conn.cursor()
-cur.execute("SELECT * FROM crew LIMIT 10")
-query_results = cur.fetchall()
-print(query_results)
+# cur.execute("SELECT * FROM crew LIMIT 10")
+# query_results = cur.fetchall()
+# print(query_results)
