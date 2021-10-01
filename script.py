e78ee1e157e32e719da42e242ba75f712f0e9e40
Furkan Sahin <furkansahin824@gmail.com>
2021-10-01 14:36:15 -0500

    can connect to psql server and execute commands now

--- script.py
+++ script.py
@@ -1,16 +1,14 @@
 import psycopg2
 
 # 315_psql='psql -h csce-315-db.engr.tamu.edu -U csce315_913_3_user -d csce315_913_3_db'
-
 conn = psycopg2.connect(
-    host="localhost",
-    database="suppliers",
-    user="postgres",
-    password="Abcd1234"
+    host="csce-315-db.engr.tamu.edu",
+    database="csce315_913_3_db",
+    user="csce315_913_3_user",
+    password="sikewrongnumber"
 )
 
-# import numpy as np
-# a = np.arange(15).reshape(3, 5)s
-# print(a)
-
-print("\n\ntesting\n")
+cur = conn.cursor()
+cur.execute("SELECT * FROM crew LIMIT 10")
+query_results = cur.fetchall()
+print(query_results)
