25e9cb6a727633c22c1aac67ad88c97a487deb70
Furkan Sahin <furkansahin824@gmail.com>
2021-10-01 13:50:15 -0500

    working psycopg2

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
-# pip install psycopg2
-
 import psycopg2
+
+# 315_psql='psql -h csce-315-db.engr.tamu.edu -U csce315_913_3_user -d csce315_913_3_db'
 
 conn = psycopg2.connect(
     host="localhost",
@@ -8,3 +8,9 @@ conn = psycopg2.connect(
     user="postgres",
     password="Abcd1234"
 )
+
+# import numpy as np
+# a = np.arange(15).reshape(3, 5)s
+# print(a)
+
+print("\n\ntesting\n")
